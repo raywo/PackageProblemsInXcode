7f3d2e6d76187f07746987f6eaa44c20ddda4806
Ray Wojciechowski <ray@skillbird.de>
2021-11-02 12:38:17 +0100

    Add WatchKit app target

---
+++ Package Test watchOS WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Package Test watchOS WatchKit Extension
+//
+//  Created by Ray Wojciechowski on 02.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Package Test watchOS WatchKit Extension/Package_Test_iOSApp.swift
@@ -0,0 +1,19 @@
+//
+//  Package_Test_iOSApp.swift
+//  Package Test watchOS WatchKit Extension
+//
+//  Created by Ray Wojciechowski on 02.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Package_Test_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}

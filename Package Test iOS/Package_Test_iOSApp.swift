259d138936971352cc8112ff0f04a78e75106723
Ray Wojciechowski <ray@skillbird.de>
2021-11-02 12:30:41 +0100

    Initial Commit

---
+++ Package Test iOS/Package_Test_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Package_Test_iOSApp.swift
+//  Package Test iOS
+//
+//  Created by Ray Wojciechowski on 02.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Package_Test_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
